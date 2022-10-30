3319e1544a375678957de0f83034028c956406cd
Zakariye Ali Alim <sakariyecali15@gmail.com>
2022-10-30 14:30:33 +0300

    Fast commit

---
+++ ReadMe.txt
@@ -0,0 +1,9 @@
+ 
+Read Me
+--------
+
+Note for running this project:
+1. Copy the project path folder 
+2. On Python Command Prompt, change the default application path to your  project path folder
+3. Run the DeliveryRobot.py file on C:\Users\\DeliveryRobot>ipython -i DeliveryRobot.py
+enjoy
---
+++ utilities.py
@@ -0,0 +1,57 @@
+
+class Displayable(object):
+    max_display_level = 1   # can be overridden in subclasses
+
+    def display(self,level,*args,**nargs):
+        """print the arguments if level is less than or equal to the
+        current max_display_level.
+        level is an integer.
+        the other arguments are whatever arguments print can take.
+        """
+        if level <= self.max_display_level:
+            print(*args, **nargs)  ##if error you are using Python2 not Python3
+
+def visualize(func):
+    return func
+import random
+
+def argmax(gen):
+    """gen is a generator of (element,value) pairs, where value is a real.
+    argmax returns an element with maximal value.
+    If there are multiple elements with the max value, one is returned at random.
+    """
+    maxv = float('-Infinity')       # negative infinity
+    maxvals = []      # list of maximal elements
+    for (e,v) in gen:
+        if v>maxv:
+            maxvals,maxv = [e], v
+        elif v==maxv:
+            maxvals.append(e)
+    return random.choice(maxvals)
+
+# Try:
+# argmax(enumerate([1,6,3,77,3,55,23]))
+
+def flip(prob):
+    """return true with probability prob"""
+    return random.random() < prob
+
+def dict_union(d1,d2):
+    """returns a dictionary that contains the keys of d1 and d2.
+    The value for each key that is in d2 is the value from d2,
+    otherwise it is the value from d1.
+    This does not have side effects.
+    """
+    d = dict(d1)    # copy d1
+    d.update(d2)
+    return d
+
+def test():
+    """Test part of utilities"""
+    assert argmax(enumerate([1,6,55,3,55,23])) in [2,4]
+    assert dict_union({1:4, 2:5, 3:4},{5:7, 2:9}) == {1:4, 2:9, 3:4, 5:7} 
+    print("Passed unit test in utilities")
+
+if __name__ == "__main__":
+    test()
+
